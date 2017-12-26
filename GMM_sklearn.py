9b995cf73b6017672d20119474dbe8709867341e
yoyololicon <ya70201@gmail.com>
2017-12-26 23:03:01 +0800

    finalizing

--- GMM_sklearn.py
+++ GMM_sklearn.py
@@ -5,7 +5,7 @@ from sklearn.mixture import GaussianMixture as GMM
 from prettytable import PrettyTable
 from datetime import datetime
 
-parser = argparse.ArgumentParser(description='ML HW3 - k-means & GMM')
+parser = argparse.ArgumentParser(description='ML HW3 - GMM')
 parser.add_argument('img', help='hw3_img.jpg')
 parser.add_argument('k', type=int)
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     img = Image.open(args.img)
     img.load()
     img.show(title='origin')
-    data = np.asarray(img, dtype='float')/255
+    data = np.asarray(img, dtype='float')
     m, n, l = data.shape
     data = np.reshape(data, (-1, l))
     k = args.k
@@ -25,14 +25,15 @@ if __name__ == '__main__':
     print 'Time cost :', b-a
 
     indice = gmm.predict(data)
-    new_data = np.round(gmm.means_[indice] * 255)
+    new_data = np.round(gmm.means_[indice])
     disp = Image.fromarray(new_data.reshape(m, n, l).astype('uint8'))
     disp.show(title='GMM')
 
     table = PrettyTable()
     table.add_column("GMM mean value", range(k))
-    table.add_column("r", np.round(gmm.means_[:, 0] * 255).astype('int'))
-    table.add_column("g", np.round(gmm.means_[:, 1] * 255).astype('int'))
-    table.add_column("b", np.round(gmm.means_[:, 2] * 255).astype('int'))
+    table.add_column("r", np.round(gmm.means_[:, 0]).astype('int'))
+    table.add_column("g", np.round(gmm.means_[:, 1]).astype('int'))
+    table.add_column("b", np.round(gmm.means_[:, 2]).astype('int'))
     print table
-    print gmm.n_iter_, gmm.lower_bound_
+    print gmm.n_iter_, 'of iterations'
+    print 'log likelihood', gmm.lower_bound_
--- k-means_sklearn.py
+++ k-means_sklearn.py
@@ -2,22 +2,36 @@ import argparse
 import Image
 import numpy as np
 from sklearn.cluster import KMeans
+from datetime import datetime
+from prettytable import PrettyTable
 
-parser = argparse.ArgumentParser(description='ML HW3 - k-means & GMM')
+parser = argparse.ArgumentParser(description='ML HW3 - k-means')
 parser.add_argument('img', help='hw3_img.jpg')
+parser.add_argument('k', type=int)
 
 if __name__ == '__main__':
     args = parser.parse_args()
     img = Image.open(args.img)
     img.load()
     img.show(title='origin')
-    data = np.asarray(img, dtype='float')/255
-    m, n = data.shape[:2]
-    data = np.reshape(data, (-1, 3))
+    data = np.asarray(img, dtype='float')
+    m, n, l = data.shape
+    data = np.reshape(data, (-1, l))
+    k = args.k
 
-    for k in (2, 3, 5, 20):
-        kmeans = KMeans(n_clusters=k, n_jobs=-1).fit(data)
-        indice = kmeans.predict(data)
-        new_data = kmeans.cluster_centers_[indice] * 255
-        disp = Image.fromarray(new_data.reshape(m, n, 3).astype('uint8'))
-        disp.show(title='k = %d' % k)
+    a = datetime.now().replace(microsecond=0)
+    kmeans = KMeans(n_clusters=k, n_jobs=-1).fit(data)
+    b = datetime.now().replace(microsecond=0)
+    print 'Time cost :', b-a
+
+    indice = kmeans.predict(data)
+    new_data = np.round(kmeans.cluster_centers_[indice])
+    disp = Image.fromarray(new_data.reshape(m, n, l).astype('uint8'))
+    disp.show(title='k-means')
+
+    table = PrettyTable()
+    table.add_column("k-means mean value", range(k))
+    table.add_column("r", np.round(kmeans.cluster_centers_[:, 0]).astype('int'))
+    table.add_column("g", np.round(kmeans.cluster_centers_[:, 1]).astype('int'))
+    table.add_column("b", np.round(kmeans.cluster_centers_[:, 2]).astype('int'))
+    print table
